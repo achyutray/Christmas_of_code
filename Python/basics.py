e6e8fe0f563104155f435ce0e2f87756c15dfc0c
Achyut Ray <achyutrai99@gmail.com>
2021-12-28 02:47:48 +0100

    Basics of Python nailed

---
+++ Python/basics.py
@@ -0,0 +1,31 @@
+# Printing
+# Hot tip: Use two "#" symbols followed immediately by text (no blank space) to write a comment block
+# For example:
+
+##
+# This was generated using the double hashtag followed immediately by text method
+ 
+print("Hello World")
+print("Hello again")
+print("I like typing this")
+print("This is fun")
+print("Yay! Printing!")
+print("I'd much rather you 'not'")
+print('I "said" do not touch this')
+
+print("Roosters", 100 -(25*3)%4)
+
+# Variables and Names in Python
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars * space_in_a_car
+average_passenger_per_car = passengers / cars_driven
+
+print("There are", cars, "available cars")
+print("There are only", drivers, "drivers available")
+print("We can transport", carpool_capacity, "people today")
+print("We need to put about", average_passenger_per_car, "in each car")
